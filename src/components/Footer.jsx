a5c5f0f4565427aedaa677db4f8b4601eeb0f445
noorulmobin <noorulmobin0011@gmail.com>
2024-06-08 00:46:32 +0500

    footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import myLocalImage63 from "../assets/insta.jpg";
 import myLocalImage64 from "../assets/twiter.jpg";
 import myLocalImage65 from "../assets/youtube.jpg";
 import myLocalImage66 from "../assets/linkdin.jpg";
-import logo from "../assets/logo.png"; // Assuming you have a logo.png in your assets
-
+import logo from "../assets/logo.png";
+import safeguardImage from "../assets/safeguard.jpg";
 const Footer = () => {
   return (
     <Box
@@ -18,29 +18,39 @@ const Footer = () => {
         margin: "auto",
       }}
     >
-      <Container maxWidth="1400">
+      <Container maxWidth="1450">
         <Grid container spacing={4}>
-          <Grid item xs={10} sm={6} md={2}>
-            <img
-              src={logo}
-              alt="Logo"
-              style={{
-                display: "block",
-                margin: "0 auto",
-                marginBottom: "16px",
-              }}
-            />
-
+          <Grid item xs={12} sm={6} md={2}>
+            <div id="logo">
+              <img
+                src={logo}
+                alt="Logo"
+                style={{
+                  display: "block",
+                  margin: "0 auto",
+                  marginBottom: "16px",
+                  maxWidth: "150px",
+                }}
+              />
+              <Typography
+                variant="h3"
+                color="textSecondary"
+                align="center"
+                gutterBottom
+              >
+                Cashify
+              </Typography>
+            </div>
             <Typography variant="h6" color="textPrimary" gutterBottom>
               Follow us on
             </Typography>
-            <Grid container spacing={1}>
+            <Grid container spacing={1} justifyContent="center">
               <Grid item>
                 <Link href="#">
                   <img
                     src={myLocalImage62}
                     alt="Facebook"
-                    style={{ width: 30, height: 30 }}
+                    style={{ width: 35, height: 35 }}
                   />
                 </Link>
               </Grid>
@@ -49,7 +59,7 @@ const Footer = () => {
                   <img
                     src={myLocalImage63}
                     alt="Instagram"
-                    style={{ width: 30, height: 30 }}
+                    style={{ width: 35, height: 35 }}
                   />
                 </Link>
               </Grid>
@@ -58,7 +68,7 @@ const Footer = () => {
                   <img
                     src={myLocalImage64}
                     alt="Twitter"
-                    style={{ width: 30, height: 30 }}
+                    style={{ width: 35, height: 35 }}
                   />
                 </Link>
               </Grid>
@@ -67,7 +77,7 @@ const Footer = () => {
                   <img
                     src={myLocalImage65}
                     alt="YouTube"
-                    style={{ width: 30, height: 30 }}
+                    style={{ width: 35, height: 35 }}
                   />
                 </Link>
               </Grid>
@@ -76,15 +86,13 @@ const Footer = () => {
                   <img
                     src={myLocalImage66}
                     alt="LinkedIn"
-                    style={{ width: 30, height: 0 }}
+                    style={{ width: 35, height: 35 }}
                   />
                 </Link>
               </Grid>
             </Grid>
           </Grid>
-          <Typography variant="h3">Cashify</Typography>
-
-          <Grid item xs={10} sm={6} md={2}>
+          <Grid item xs={12} sm={6} md={2}>
             <Typography variant="h6" color="textPrimary" gutterBottom>
               Services
             </Typography>
@@ -113,7 +121,7 @@ const Footer = () => {
               ))}
             </ul>
           </Grid>
-          <Grid item xs={10} sm={6} md={2}>
+          <Grid item xs={12} sm={6} md={2}>
             <Typography variant="h6" color="textPrimary" gutterBottom>
               Company
             </Typography>
@@ -137,7 +145,7 @@ const Footer = () => {
               ))}
             </ul>
           </Grid>
-          <Grid item xs={10} sm={6} md={2}>
+          <Grid item xs={12} sm={6} md={2}>
             <Typography variant="h6" color="textPrimary" gutterBottom>
               Sell Device
             </Typography>
@@ -160,7 +168,7 @@ const Footer = () => {
               ))}
             </ul>
           </Grid>
-          <Grid item xs={10} sm={6} md={2}>
+          <Grid item xs={12} sm={6} md={2}>
             <Typography variant="h6" color="textPrimary" gutterBottom>
               Help & Support
             </Typography>
@@ -179,7 +187,7 @@ const Footer = () => {
               )}
             </ul>
           </Grid>
-          <Grid item xs={10} sm={6} md={2}>
+          <Grid item xs={12} sm={6} md={2}>
             <Typography variant="h6" color="textPrimary" gutterBottom>
               More Info
             </Typography>
@@ -208,29 +216,56 @@ const Footer = () => {
         </Grid>
         <Box mt={6}>
           <Typography
-            variant="body2"
+            variant="h6"
             color="textSecondary"
-            align="center"
+            align="left"
             sx={{ mt: 4 }}
           >
-            {
-              "Registered Office: Manak Waste Management Pvt Ltd., B-39, 1st Floor, Middle Circle, Connaught Place, New Delhi-110001, India, Support-7290068900 | CIN: U27205DL2009PTC190441 "
-            }
+            Registered Office:
           </Typography>
-          <Typography variant="body2" color="textSecondary" align="center">
-            {
-              "Manak Waste Management Pvt Ltd. is ISO 27001 & 27701 Compliance Certified. Person who may be contacted in case of any compliance related queries or grievances: Manoj Kumar (manoj.k@cashify.in)"
-            }
+          <Typography variant="body1" color="textSecondary">
+            Manak Waste Management Pvt Ltd., B-39, 1st Floor, Middle Circle,
+            Connaught Place, New Delhi-110001, India, Support-7290068900 | CIN:
+            U27205DL2009PTC190441 "
           </Typography>
-          <Typography variant="body2" color="textSecondary" align="center">
-            {
-              "** All product names, logos, and brands are property of their respective owners. All company, product and service names used in this website are for identification purposes only. Use of these names, logos, and brands does not imply endorsement."
-            }
+          <Typography variant="body1" color="textSecondary" align="left">
+            Manak Waste Management Pvt Ltd. is ISO 27001 & 27701 Compliance
+            Certified. Person who may be contacted in case of any compliance{" "}
+            <br /> related queries or grievances: Manoj Kumar <br />
+            (manoj.k@cashify.in)"
           </Typography>
           <Typography
-            variant="body2"
+            variant="body1"
             color="textSecondary"
-            align="center"
+            align="left"
+            marginTop="10px"
+          >
+            ** All product names, logos, and brands are property of their
+            respective owners. All company, product and service names used in
+            this <br /> website are for identification purposes only. Use of
+            these names, logos, and brands does not imply endorsement."
+          </Typography>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            mt={4}
+          >
+            <Typography variant="body1" color="textSecondary">
+              {
+                "Safeguarded by DeviceSafety.org\nAll devices are data-wiped using DeviceSafety.org certified tools, guaranteeing the highest standards of data security and privacy."
+              }
+            </Typography>
+            <img
+              src={safeguardImage}
+              alt="Safeguard by DeviceSafety.org"
+              style={{ width: "400px", marginLeft: "100px" }}
+            />
+          </Box>
+          <Typography
+            variant="body1"
+            color="textSecondary"
+            align="left"
             sx={{ mt: 2 }}
           >
             {"Copyright © 2024 Cashify All rights reserved"}
